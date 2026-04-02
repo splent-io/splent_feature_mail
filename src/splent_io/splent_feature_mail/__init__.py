50bc6d8a54f57f7315a329b6cc7b5f34a239bc5f
drorganvidez <davromorg@alum.us.es>
2026-04-02 01:32:47 +0000

    chore: release splent_io/splent_feature_mail to v1.3.5

--- src/splent_io/splent_feature_mail/__init__.py
+++ src/splent_io/splent_feature_mail/__init__.py
@@ -1,14 +1,13 @@
-from splent_framework.blueprints.base_blueprint import BaseBlueprint
+from splent_framework.blueprints.base_blueprint import create_blueprint
+from splent_framework.services.service_locator import register_service
 
-mail_bp = BaseBlueprint("mail", __name__, template_folder="templates")
+from splent_io.splent_feature_mail.services import MailService
+
+mail_bp = create_blueprint(__name__)
 
 
 def init_feature(app):
-    from splent_io.splent_feature_mail.services import MailService
-
-    mail_service = MailService()
-    mail_service.init_app(app)
-    app.extensions["splent_mail_service"] = mail_service
+    register_service(app, "MailService", MailService)
 
 
 def inject_context_vars(app):
